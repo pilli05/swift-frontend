e6cdfe8d293a783c6156d18675a9cfdc7c45dcaf
pilli05 <pillirajesh@ymail.com>
2025-07-09 11:36:41 +0530

    pagination and sorting added

--- swift-frontend/src/pages/Dashboard/Dashboard.tsx
+++ swift-frontend/src/pages/Dashboard/Dashboard.tsx
@@ -12,6 +12,9 @@ interface Comments {
 
 const Dashboard = () => {
   const [comments, setComments] = useState<Comments[]>([]);
+  const [inputSearch, setInputSearch] = useState("");
+  const [sortBy, setSortBy] = useState<keyof Comments | null>(null);
+  const [sortOrder, setSortOrder] = useState("asc");
   const [currentPage, setCurrentPage] = useState(1);
   const [pageSize, setPageSize] = useState(10);
 
@@ -26,17 +29,66 @@ const Dashboard = () => {
     getCommentsList();
   }, []);
 
+  const handleSort = (sortKey: keyof Comments) => {
+    if (sortKey === sortBy) {
+      setSortOrder((prev) => (prev === "asc" ? "dsc" : "asc"));
+    } else {
+      setSortBy(sortKey);
+      setSortOrder("asc");
+    }
+  };
+
+  const filteredResults = comments.filter((comment: Comments) =>
+    [comment.name, comment.email, comment.body]
+      .join(" ")
+      .toLowerCase()
+      .includes(inputSearch.toLowerCase())
+  );
+
+  const startIndex = (currentPage - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+
+  let paginatedData = filteredResults.slice(startIndex, endIndex);
+
+  if (sortBy) {
+    paginatedData = [...paginatedData].sort((commentA, commentB) => {
+      const valueA = commentA[sortBy];
+      const valueB = commentB[sortBy];
+
+      if (typeof valueA === "string" && typeof valueB === "string") {
+        return sortOrder === "asc"
+          ? valueA.localeCompare(valueB)
+          : valueB.localeCompare(valueA);
+      }
+
+      if (typeof valueA === "number" && typeof valueB === "number") {
+        return sortOrder === "asc" ? valueA - valueB : valueB - valueA;
+      }
+
+      return 0;
+    });
+  }
+
   return (
     <div className="!px-5 md:!px-20 !py-10">
       <div className="grid grid-cols-12  gap-4">
         <div className="col-span-12 md:col-span-9 flex items-center font-[500] !space-x-4 text-sm text-[#1b1e3e]">
-          <span className="shadow !px-3 !py-1 rounded-md border border-gray-200 flex items-center cursor-pointer">
+          <span
+            className="shadow !px-3 !py-1 rounded-md border border-gray-200 flex items-center cursor-pointer"
+            onClick={() => handleSort("postId")}
+          >
             Sort Post ID <FaSort className="!ml-2" color="#505377" />
           </span>
-          <span className="shadow !px-3 !py-1 rounded-md border border-gray-200 flex items-center cursor-pointer">
+          <span
+            className="shadow !px-3 !py-1 rounded-md border border-gray-200 flex items-center cursor-pointer"
+            onClick={() => handleSort("name")}
+          >
             Sort Name <FaSort className="!ml-2" color="#505377" />
           </span>
-          <span className="shadow !px-3 !py-1 rounded-md border border-gray-200 flex items-center cursor-pointer">
+          <span
+            className="shadow !px-3 !py-1 rounded-md border border-gray-200 flex items-center cursor-pointer"
+            onClick={() => handleSort("email")}
+          >
             Sort Email <FaSort className="!ml-2" color="#505377" />
           </span>
         </div>
@@ -44,6 +96,10 @@ const Dashboard = () => {
           type="search"
           placeholder="Search name, email, comment"
           className="!px-2 !py-1 outline-none rounded-md border border-gray-300 shadow col-span-12 md:col-span-3"
+          onChange={(e) => {
+            setInputSearch(e.target.value);
+            setCurrentPage(1);
+          }}
         />
       </div>
       <div className="!mt-10 !rounded-md overflow-x-auto">
@@ -61,24 +117,22 @@ const Dashboard = () => {
             </tr>
           </thead>
           <tbody className="bg-white text-sm font-semibold text-[#1b1e3e]">
-            {comments && comments.length > 0 ? (
-              comments
-                .slice((currentPage - 1) * pageSize, currentPage * pageSize)
-                .map((comment) => (
-                  <tr
-                    className="text-sm font-semibold  border-b-2 border-gray-200"
-                    key={comment.id}
-                  >
-                    <td className="!px-2 !py-2  !rounded-bl-[5px]">
-                      {comment.postId}
-                    </td>
-                    <td className="!px-2 !py-2 ">{comment.name}</td>
-                    <td className="!px-2 !py-2 ">{comment.email}</td>
-                    <td className="!px-2 !py-2  !rounded-br-[5px]">
-                      {comment.body}
-                    </td>
-                  </tr>
-                ))
+            {paginatedData && paginatedData.length > 0 ? (
+              paginatedData.map((comment) => (
+                <tr
+                  className="text-sm font-semibold  border-b-2 border-gray-200"
+                  key={comment.id}
+                >
+                  <td className="!px-2 !py-2  !rounded-bl-[5px]">
+                    {comment.postId}
+                  </td>
+                  <td className="!px-2 !py-2 ">{comment.name}</td>
+                  <td className="!px-2 !py-2 ">{comment.email}</td>
+                  <td className="!px-2 !py-2  !rounded-br-[5px]">
+                    {comment.body}
+                  </td>
+                </tr>
+              ))
             ) : (
               <tr>
                 <td colSpan={12} className="py-5 text-center">
@@ -92,7 +146,9 @@ const Dashboard = () => {
       <div className="!mt-10 flex items-center justify-end !space-x-4 text-sm text-[#1b1e3e] ">
         <span>{`${
           (currentPage - 1) * pageSize === 0 ? 1 : (currentPage - 1) * pageSize
-        }-${currentPage * pageSize} of ${comments.length}  items`}</span>
+        }-${currentPage * pageSize} of ${
+          filteredResults?.length
+        }  items`}</span>
         {(currentPage - 1) * pageSize !== 0 && (
           <FaAngleLeft
             className="cursor-pointer"
@@ -100,21 +156,25 @@ const Dashboard = () => {
           />
         )}{" "}
         <span className="flex items-center  w-44 overflow-x-auto pagination-scrollbar">
-          {[...Array(comments.length / 10)].map((_, index: number) => (
-            <span
-              className={
-                index + 1 === currentPage
-                  ? "border border-[#1b1e3e] !mr-2 !px-1 rounded-sm cursor-pointer"
-                  : "cursor-pointer !mr-2 !px-1 rounded-sm"
-              }
-              onClick={() => setCurrentPage(index + 1)}
-              key={index}
-            >
-              {index + 1}
-            </span>
-          ))}
+          {[...Array(Math.ceil(filteredResults.length / pageSize))].map(
+            (_, index: number) => (
+              <span
+                className={
+                  index + 1 === currentPage
+                    ? "border border-[#1b1e3e] !mr-2 !px-1 rounded-sm cursor-pointer"
+                    : "cursor-pointer !mr-2 !px-1 rounded-sm"
+                }
+                onClick={() => {
+                  setCurrentPage(index + 1);
+                }}
+                key={index}
+              >
+                {index + 1}
+              </span>
+            )
+          )}
         </span>
-        {currentPage !== comments.length / pageSize && (
+        {currentPage !== filteredResults.length / pageSize && (
           <FaAngleRight
             className="cursor-pointer"
             onClick={() => setCurrentPage(currentPage + 1)}
